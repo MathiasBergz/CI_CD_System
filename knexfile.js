6680411046221a2e7f192570172e3433a5b3f826
MathiasBergz <mathias15052004@gmail.com>
2024-12-01 22:53:47 -0300

    Setting default files

---
+++ knexfile.js
@@ -0,0 +1,19 @@
+module.exports = {
+    production: {
+        client: 'pg',
+        connection: {
+          host: 'aws-0-sa-east-1.pooler.supabase.com',
+          port: 6543,
+          database: 'postgres',
+          user: 'postgres.oxzbqtkdwwcoutyicwro',
+          password: 'Audithorium!1505',
+        },
+        migrations: {
+          tableName: 'knex_migrations',
+          directory: './migrations',
+        },
+        seeds: {
+          directory: './seeds',
+        },
+      },
+}
