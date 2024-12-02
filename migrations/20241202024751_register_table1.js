9e49217c6e10fdf440e7176c20008b1658dc3c50
MathiasBergz <mathias15052004@gmail.com>
2024-12-01 23:49:25 -0300

    Created migration file

---
+++ migrations/20241202024751_register_table1.js
@@ -0,0 +1,24 @@
+exports.up = function(knex) {
+    return knex('registro')
+      .insert([
+        { name: 'Registro2', version: '1.0.1' }
+      ])
+      .then(() => {
+        console.log('Registros inseridos com sucesso.');
+      })
+      .catch((err) => {
+        console.error('Erro ao inserir os registros:', err);
+      });
+  };
+  
+  exports.down = function(knex) {
+    return knex('registro')
+      .whereIn('name', ['Registro2'])  // Exclui os registros inseridos
+      .del()
+      .then(() => {
+        console.log('Registros deletados com sucesso.');
+      })
+      .catch((err) => {
+        console.error('Erro ao deletar os registros:', err);
+      });
+  };
