565235a046c4d8f674d85c0c17cb2dfac736084a
MathiasBergz <mathias15052004@gmail.com>
2024-12-01 23:19:17 -0300

    Second test with workflow and migrations

---
+++ migrations/20241202021739_create_table.js
@@ -0,0 +1,13 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('registro', function(table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('version');
+      table.timestamps();
+    });
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('registro');
+  };
+  
